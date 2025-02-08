767667c3b9a60ea54bb2da6ade7f5af235322777
mdnur3330 <hmnuralomseddeki@gmail.com>
2025-02-08 20:08:14 +0600

    result valo hele mojar sujog hobe nole hobena

---
+++ array.js
@@ -0,0 +1,30 @@
+// result valo hele mojar sujog hobe nole hobena
+
+let myMarks = 3;
+let fMarks = 3;
+
+if(myMarks>=80 && fMarks >= 80){
+    console.log('we will go to the chg for lanc');
+    
+}else{
+    if(myMarks<80){
+        console.log('going to caf for eating prohebeted for me');
+        
+    }else{
+        if(fMarks>=60 && fMarks<80){
+            console.log('good luck next time jabo');
+            
+        }else{
+            if(fMarks>= 40 && fMarks< 60){
+                console.log('his massege will be unsend');
+                
+            }else{
+                if(fMarks<40){
+                    console.log('he will be blockd from my facebook');
+                    
+                }
+            }
+        }
+    }
+}
+
